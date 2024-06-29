5cd367a6be6a6a905ed06697859080fb24d0f875
tanish <tbhatia0225@gmail.com>
2024-06-29 15:23:43 +0530

    Database Connected , 2 din laggye karne me

--- ai-interview-mocker/drizzle.config.js
+++ ai-interview-mocker/drizzle.config.js
@@ -1,9 +1,16 @@
 /** @type { import("drizzle-kit").Config } */
-export default {
+import { defineConfig } from "drizzle-kit";
+
+
+export default defineConfig ({
 	schema: "utils/schema.js",
 	dialect: 'postgresql',
+	
 	dbCredentials: {
-	  url: 'postgresql://neondb_owner:F6sS7YZgEAoU@ep-wandering-mountain-a5y2uybi-pooler.us-east-2.aws.neon.tech/mock-interview?sslmode=require',
+		url: "postgresql://neondb_owner:F6sS7YZgEAoU@ep-wandering-mountain-a5y2uybi-pooler.us-east-2.aws.neon.tech/mock-interview?sslmode=require"
 	}
-  };
+
+
+})
+
   
--- ai-interview-mocker/utils/schema.js
+++ ai-interview-mocker/utils/schema.js
@@ -1,12 +1,12 @@
 import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
 
-export const MockInterview = pgTable('MockInterview', {
-  id: serial("id").primaryKey(),
-  jsonMockResp: text().notNull(),
-  jobPosition: varchar().notNull(),
-  jobDesc: varchar().notNull(),
-  jobExperience: varchar().notNull(),
-  createdBy: varchar().notNull(),
-  createdAt: varchar().notNull(),
-  mockId: varchar().notNull(),
+export const MockInterview = pgTable('mockInterview', {
+    id: serial("id").primaryKey(),
+    jsonMockResp: text('jsonMockResp').notNull(),
+    jobPosition: varchar('jobPosition').notNull(),
+    jobDesc: varchar('jobDesc').notNull(),
+    jobExperience: varchar('jobExperience').notNull(),
+    createdBy: varchar('createdBy').notNull(),
+    createdAt: varchar('createdAt').notNull(),
+    mockId: varchar('mockId').notNull(),
 });
